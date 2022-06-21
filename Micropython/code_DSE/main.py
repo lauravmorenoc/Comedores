477f6cce07ac546c0478e377cb80220f763964db
jforerob <jforerob@unal.edu.co>
2022-06-21 16:16:11 -0500

    Buttons and audio union to main code

--- Micropython/code_DSE/main.py
+++ Micropython/code_DSE/main.py
@@ -5,7 +5,10 @@ from ili9341 import Display, color565
 from machine import Pin, SPI
 from xglcd_font import XglcdFont
 from machine import SoftSPI
+from wavplayer import WavPlayer
 import math
+import os
+
 
 sck=22
 mosi=19
@@ -28,6 +31,25 @@ cash_register_on=False
 ID=''
 IDcounter=0
 
+SCK_PIN = 26 #32
+WS_PIN = 22 #25
+SD_PIN = 21 #33
+I2S_ID = 0
+BUFFER_LENGTH_IN_BYTES = 5000
+global local_ticket
+local_ticket=0
+last_ticket=0
+
+wp = WavPlayer(id=I2S_ID,
+              sck_pin=Pin(SCK_PIN),
+              ws_pin=Pin(WS_PIN),
+              sd_pin=Pin(SD_PIN),
+              ibuf=BUFFER_LENGTH_IN_BYTES)
+#---- AUDIO CONFIG
+
+push_button = Pin(25, Pin.IN)  # 23 number pin is input
+bw_button = Pin(23, Pin.IN) 
+
 def char_type(char):
     if char=='1' or char=='2' or char=='3' or char=='4' or char=='5' or char=='6' or char=='7' or char=='8' or char=='9' or char=='0':
         return 'Number'
@@ -35,6 +57,26 @@ def char_type(char):
         return 'Ast'
     elif char=='B': #Numeral
         return 'Sharp'
+    
+def button_verify():
+    
+    global local_ticket
+
+    logic_state = push_button.value()
+    bw_logic_state= bw_button.value()
+  
+    if logic_state == True or bw_logic_state== True:     # if pressed the push_button
+    
+        if bw_logic_state == True:
+            local_ticket=local_ticket-1
+            print("if")
+            print(local_ticket)
+        else:
+            local_ticket = local_ticket+1
+            led.value(1)
+            print("else")
+            print(local_ticket)
+    
 
 while True:    
     
@@ -55,6 +97,17 @@ while True:
     except OSError as e:
         print('Unable to connect. Please restart device.')
     ####
+
+# Send messages
+
+    button_verify()
+    if local_ticket!=last_ticket:
+        try:
+            COMM.send(topic='Easymeals/Update', ticket=sim_ticket, comedor=comedor)
+        except OSError as e:
+            print('OSError: Unable to sent ticket update. Please restart device.')
+        last_ticket=local_ticket
+    
     
 # Receive messages
     topic, message, pending_incoming_message=COMM.receive()
@@ -84,24 +137,45 @@ while True:
                 disp.printText(str(ticket), vspace=3, hspace=11)
                 cash_register_on=False
                 
-                # Delete last users'data from display
-                disp.printText('      ', vspace=5, hspace=1)
-                disp.printText('                     ', vspace=6, hspace=1)
-                disp.printText('Digite su numero de identificacion:', vspace=8, hspace=1)
-                disp.printText('            ', vspace=9, hspace=1)
-                disp.printText('                     ', vspace=11, hspace=1)
-                disp.printText('     ', vspace=12, hspace=8)
+                wp.play("turno.wav", loop= False)
+                done_deleting=False
+                while wp.isplaying() == True or done_deleting==False:
+                    # Delete last users'data from display
+                    disp.printText('      ', vspace=5, hspace=1)
+                    disp.printText('                     ', vspace=6, hspace=1)
+                    disp.printText('Digite su numero de identificacion:', vspace=8, hspace=1)
+                    disp.printText('            ', vspace=9, hspace=1)
+                    disp.printText('                     ', vspace=11, hspace=1)
+                    disp.printText('     ', vspace=12, hspace=8)
+                    done_deleting==True
+                    pass
                 
-                # Poner aquí función de que se reproduzca en audio
+                num_file = str(ticket) + ".wav"
+                
+                if ticket <= 9:
+                    wp.play(num_file, loop= False)
+                    while wp.isplaying() == True: 
+                        pass
+                elif ticket >= 10 and ticket <= 99:
+                    tens = ticket // 10
+                    unit = ticket % 10
+                    wp.play(str(tens) + ".wav", loop= False)
+                    while wp.isplaying() == True:
+                        pass
+                    wp.play(str(unit) + ".wav", loop= False)
+                    while wp.isplaying() == True: 
+                        pass
             
         COMM.pending_incoming_message=False    
             
         
-    # Sent messages
+    # Send messages
+    
+    
     
     # La función de Juan debe tener un print. Cambiar ese print por, considerando 'out' lo que se imprime:
 
-
+'''
     if !cash_register_on: # Awaiting to get key from keyboard
         if char_type(out) =='Number':
             disp.printText(out,vspace=9, hspace=IDcounter)
@@ -120,4 +194,4 @@ while True:
                 cash_register_on=True
             except OSError as e:
                 print('OSError: Unable to send ID')
-
+'''
